37ff40d0c75e4534bb0c255c220f55f58c4b03b7
Thurzin11 <arthursantosgoncalves587@gmail.com>
2024-11-12 16:18:28 -0300

    att

--- JavaScript/arrays/funcoesAvancadas.js
+++ JavaScript/arrays/funcoesAvancadas.js
@@ -1,43 +1,43 @@
-// function prepararPedido(numeroPedido, callback) {
-//     console.log(`Preparando pedido #${numeroPedido}`);
+function prepararPedido(numeroPedido, callback) {
+    console.log(`Preparando pedido #${numeroPedido}`);
     
-//     // Simulando tempo de preparo
-//     setTimeout(() => {
-//         const pedido = {
-//             numero: numeroPedido,
-//             status: 'pronto'
-//         };
-//         callback(pedido);
-//     }, 3000);
-// }
+    // Simulando tempo de preparo
+    setTimeout(() => {
+        const pedido = {
+            numero: numeroPedido,
+            status: 'pronto'
+        };
+        callback(pedido);
+    }, 3000);
+}
 
-// function notificarCliente(pedido) {
-//     console.log(`🔔 Pedido #${pedido.numero} está ${pedido.status}!`);
-// }
+function notificarCliente(pedido) {
+    console.log(`🔔 Pedido #${pedido.numero} está ${pedido.status}!`);
+}
 
-// // Usando o callback
-// prepararPedido(123, notificarCliente);
+// Usando o callback
+prepararPedido(123, notificarCliente);
 
 
 
-// function criarContadorVisitas() {
-//     let visitas = 0;
+function criarContadorVisitas() {
+    let visitas = 0;
     
-//     return {
-//         registrarVisita: function() {
-//             visitas++;
-//             return visitas;
-//         },
-//         obterTotalVisitas: function() {
-//             return visitas;
-//         }
-//     };
-// }
+    return {
+        registrarVisita: function() {
+            visitas++;
+            return visitas;
+        },
+        obterTotalVisitas: function() {
+            return visitas;
+        }
+    };
+}
 
-// const contadorBlog = criarContadorVisitas();
-// console.log(contadorBlog.registrarVisita()); // 1
-// console.log(contadorBlog.registrarVisita()); // 2
-// console.log(contadorBlog.obterTotalVisitas()); // 2
+const contadorBlog = criarContadorVisitas();
+console.log(contadorBlog.registrarVisita()); // 1
+console.log(contadorBlog.registrarVisita()); // 2
+console.log(contadorBlog.obterTotalVisitas()); // 2
 
 const usuario = {
     nome: 'João',
