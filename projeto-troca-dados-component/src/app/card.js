738dceef1c500755308f2caa704fb2005603768a
Thurzin11 <arthursantosgoncalves587@gmail.com>
2024-11-27 17:00:42 -0300

    att

---
+++ projeto-troca-dados-component/src/app/card.js
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Card(props) {
+  const [product, setProduct] = useState(props.product);
+  return (
+    <div className="hover:bg-gray-100 transition duration-200 border border-gray-300 rounded-lg shadow-lg p-4 max-w-sm bg-white">
+      <img
+        src={product.imagem}
+        alt={product.nome}
+        className="w-full h-48 rounded-t-lg mb-4"
+      />
+      <h2 className="text-lg font-semibold text-gray-800 mb-2">
+        {product.nome}
+      </h2>
+      <p className="text-gray-600 text-sm">{product.descricao}</p>
+      <div className="mt-4">
+        <span className="text-gray-800 font-bold">
+          R$ {product.valor.toFixed(2)}
+        </span>
+        <span className="text-gray-500 text-sm ml-2">
+          Estoque: {product.quantidade}
+        </span>
+      </div>
+      <div className="mt-4 flex space-x-2">
+        <button
+          onClick={() => {
+            product.quantidade--;
+            props.handleAddToCart(product);
+          }}
+          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-200"
+        >
+          Adicionar
+        </button>
+        <button
+          onClick={() => props.handleRemoveFromCart(product)}
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-200"
+        >
+          Retirar
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ projeto-troca-dados-component/src/app/cart.js
@@ -0,0 +1,42 @@
+export default function Cart(props) {
+  console.log(props.cart);
+  return (
+    <footer className="fixed w-[20vw] left-[79vw] h-[100vh] bg-black text-white p-5">
+      <div className="h-[80vh] overflow-y-auto snap-y snap-mandatory">
+        {props.cart.map((item, index) => (
+          <div
+            key={index}
+            className="hover:bg-gray-100 transition duration-200 mb-5 border border-gray-300 rounded-lg shadow-lg p-4 max-w-sm bg-white"
+          >
+            <h2 className="text-lg font-semibold text-gray-800 mb-2">
+              {item.nome}
+            </h2>
+            <div className="flex justify-between items-center">
+              <span className="text-gray-600 text-sm">
+                {item.quantidade}x R$ {item.valor.toFixed(2)}
+              </span>
+              <button
+                onClick={() => props.handleRemoveFromCart(item)}
+                className="text-red-500"
+              >
+                Remover
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col text-sm absolute top-[88vh] border">
+        <span className="font-semibold">
+          Total: R${" "}
+          {props.cart
+            .reduce((acc, item) => acc + item.valor * item.quantidade, 0)
+            .toFixed(2)}
+        </span>
+        <span className="font-semibold">
+          Itens no Carrinho:{" "}
+          {props.cart.reduce((acc, item) => acc + item.quantidade, 0)}
+        </span>
+      </div>
+    </footer>
+  );
+}
