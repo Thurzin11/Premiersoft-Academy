8f16a5f20e43fabb6b9f1bb6a4fc64df21f7e608
Thurzin11 <arthursantosgoncalves587@gmail.com>
2024-12-03 13:13:26 -0300

    update header component

--- soccer/src/app/header.tsx
+++ soccer/src/app/header.tsx
@@ -1,15 +1,15 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="flex justify-around w-[100%] h-[10vh] bg-black text-white ">
       <div className="w-[70%] flex flex-col text-center justify-center">
-        <h1 className="w-[10%] h-[50%]">Soccer</h1>
+        <h1 className="w-[10%] h-[50%]"><Link href={"/"}>Soccer</Link></h1>
       </div>
         <ul className="w-[30%] flex justify-around items-center">
-          <li>Home</li>
-          <li>News</li>
-          <li>Teams</li>
-          <li>Players</li>
-          <li><a href="/competitions">Leagues</a></li>
+          <li><Link href={"/"}>Home</Link></li>
+          <li><Link href={"/competitions/teams"}>Teams</Link></li>
+          <li><Link href="/competitions">Leagues</Link></li>
         </ul>
     </header>
   );
