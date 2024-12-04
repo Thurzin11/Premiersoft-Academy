1873ebb343910b567f5589a0a07afd6bd7180355
Thurzin11 <arthursantosgoncalves587@gmail.com>
2024-12-04 10:20:09 -0300

    update to-do-list

--- to-do-list/src/app/list/page.tsx
+++ to-do-list/src/app/list/page.tsx
@@ -1,5 +1,14 @@
 export default function List() {
-    return(
-        <div>olaoladsd</div>
-    )
+  return (
+    <div className="grid grid-cols-12">
+      <div className="flex flex-col col-span-2">
+        <label htmlFor="task">Task</label>
+        <input
+          className="border border-black placeholder:text-black px-2"
+          type="text"
+          placeholder="Insira uma Task"
+        />
+      </div>
+    </div>
+  );
 }
--- to-do-list/src/app/page.tsx
+++ to-do-list/src/app/page.tsx
@@ -5,7 +5,7 @@ import Main from "./main";
 export default function Home() {
   return (
     <>
-    <Main />
+    <Main/>
     </>
   );
 }
