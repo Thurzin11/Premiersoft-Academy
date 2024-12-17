756aaf66634c542b27ec933e806ddc5b429f7ec1
Thurzin11 <arthursantosgoncalves587@gmail.com>
2024-12-17 13:34:47 -0300

    configuration entitys done

--- exercise nest/exercise/src/app.module.ts
+++ exercise nest/exercise/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, OrdersModule, UsersModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
