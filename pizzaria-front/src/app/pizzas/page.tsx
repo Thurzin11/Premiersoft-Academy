6c378c3555fd43f4f288eeadf43272c121670a0a
Thurzin11 <arthursantosgoncalves587@gmail.com>
2025-01-09 13:32:51 -0300

    feature getPizzas is working and getCategories is almost done

--- pizzaria-front/src/app/pizzas/page.tsx
+++ pizzaria-front/src/app/pizzas/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import { getPizzas } from "@/lib/api";
+import React from "react";
 
-const ListPizzas = () => {
+const ListPizzas: React.FC = async () => {
+  const pizzas: IPizza[] = await getPizzas();
   return (
-    <div>
-      
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-6 text-center">Pizzas</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {pizzas.map((pizza) => (
+          <div
+            key={pizza.id}
+            className="border rounded-lg shadow-lg p-4 bg-white flex flex-col"
+          >
+            <h2 className="text-xl font-semibold mb-2">{pizza.name}</h2>
+            <p className="text-gray-700 mb-2">{pizza.description}</p>
+            <p className="text-gray-800 font-bold mb-2">R$ {pizza.price}</p>
+            <p
+              className={`mb-2 font-medium ${
+                pizza.isAvailable ? "text-green-600" : "text-red-600"
+              }`}
+            >
+              {pizza.isAvailable ? "Disponível" : "Indisponível"}
+            </p>
+            <div className="mt-4">
+              <h3 className="text-sm font-bold mb-2">Categorias:</h3>
+              <ul className="list-disc list-inside text-gray-600">
+                {pizza.categories.map((category) => (
+                  <li key={category.id}>
+                    <span className="font-semibold">{category.name}:</span>{" "}
+                    {category.description}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListPizzas
+export default ListPizzas;
--- pizzaria-front/src/lib/api.ts
+++ pizzaria-front/src/lib/api.ts
@@ -2,10 +2,19 @@ import axios from "axios";
 
 export async function getPizzas(): Promise<IPizza[]> {
   try {
-    const response = await axios.get<IPizza[]>(
-      `${process.env.HOST}/pizzas`,
+    const response = await axios.get<IPizza[]>(`${process.env.HOST}/pizza`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getCategories(): Promise<ICategory[]> {
+  try {
+    const response = await axios.get<ICategory[]>(
+      `${process.env.HOST}/category`
     );
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(error);
