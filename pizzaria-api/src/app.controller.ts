d8b18950067a49d5ffe7e10f1fc1658f2dda3fb5
Thurzin11 <arthursantosgoncalves587@gmail.com>
2025-01-15 13:23:14 -0300

    first configuration docker

--- pizzaria-api/src/app.controller.ts
+++ pizzaria-api/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as process from 'node:process';
+import { Public } from './auth/constants/constants';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return String(process.env.NODE_ENV);
   }
 }
